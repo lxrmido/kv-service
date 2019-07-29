1d6bc38f6764866645924e274e376d94ac0abefc
lxrmido <lxrmido@qq.com>
2019-07-29 17:08:53 +0800

    record changes

--- index.js
+++ index.js
@@ -10,21 +10,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var config = {
     servicePort: process.env.SERIVCE_PORT || 3000,
     dataDir: process.env.DATA_DIR || 'data',
-    backupFile: process.env.BACKUP_FILE || 'data/values.json',
+    backupValuesFile: process.env.BACKUP_VALUES_FILE || 'data/values.json',
+    backupChangesFile: process.env.BACKUP_CHANGES_FILE || 'data/changes.json',
     backupInterval: process.env.BACKUP_INTERVAL || 60000,
+    changesLimit: process.env.CHANGES_LIMIT || 8640
 };
 
 var valuesMap = {
 
 };
 
+var changesMap = {
+
+};
+
+var daily = {
+    dir: null,
+    key: null,
+    changes: {
+
+    }
+};
+
 if (!fs.existsSync(config.dataDir)) {
     fs.mkdirSync(config.dataDir);
 }
 
-if (fs.existsSync(config.backupFile)) {
-    valuesMap = JSON.parse(fs.readFileSync(config.backupFile));
+if (fs.existsSync(config.backupValuesFile)) {
+    valuesMap = JSON.parse(fs.readFileSync(config.backupValuesFile));
 }
+
+if (fs.existsSync(config.backupChangesFile)) {
+    changesMap = JSON.parse(fs.readFileSync(config.backupChangesFile));
+}
+
+initRotate();
 
 app.post('/set', function (req, res) {
     for (let i in req.body) {
@@ -32,6 +52,9 @@ app.post('/set', function (req, res) {
             value: req.body[i],
             updated: (new Date()).getTime()
         };
+        process.nextTick(function () {
+            addChange(i);
+        });
     }
     res.send({
         result: 'success'
@@ -49,12 +72,29 @@ app.get('/get/:key', function (req, res) {
     }
 });
 
-app.get('/raw/:key', function (req, res) {
+app.get('/changes/:key', function (req, res) {
     let key = req.params.key;
-    if (key in valuesMap) {
-        res.send(valuesMap[key].value);
+    if (key in daily.changes) {
+        res.send({
+            changes: daily.changes[key]
+        });
     } else {
-        res.send('');
+        res.send({
+            changes: []
+        });
+    }
+});
+
+app.get('/today/:key', function (req, res) {
+    let key = req.params.key;
+    if (key in changesMap) {
+        res.send({
+            changes: changesMap[key]
+        });
+    } else {
+        res.send({
+            changes: []
+        });
     }
 });
 
@@ -62,26 +102,101 @@ app.listen(config.servicePort, function () {
   console.log('Listening on port ' + config.servicePort);
 });
 
-function backup(){
+function addChange(key){
+    if (!(key in changesMap)) {
+        changesMap[key] = [];
+    } else {
+        if (changesMap[key].length >= config.changesLimit) {
+            changesMap[key].splice(0, values.length - config.changesLimit + 1);
+        }
+    }
+    changesMap[key].push(valuesMap[key]);
+    checkRotate();
+    if (!(key in daily.changes)) {
+        daily.changes[key] = [];
+    }
+    daily.changes[key].push(valuesMap[key]);
+}
+
+function getDailyKey() {
+    let d = new Date();
+    function pad(x) {
+        return (x > 9 ? '' : '0') + x;
+    }
+    return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-' + pad(d.getDate());
+}
+
+function checkRotate(){
+    let curKey = getDailyKey();
+    if (curKey != daily.key) {
+        backupDaily();
+        daily.dir = config.dataDir + '/' + curKey;
+        daily.key = curKey;
+        daily.changes = {};
+        loadRotateIfExists();
+    }
+}
+
+function initRotate(){
+    let curKey = getDailyKey();
+    daily.dir = config.dataDir + '/' + curKey + '/';
+    daily.key = curKey;
+    loadRotateIfExists();
+}
+
+function loadRotateIfExists(){
+    if (!fs.existsSync(daily.dir)) {
+        fs.mkdirSync(daily.dir);
+        return false;
+    }
+    fs.readdirSync(daily.dir).forEach(function (name) {
+        let segs = name.split('.');
+        daily.changes[segs[0]] = JSON.parse(fs.readFileSync(daily.dir + name));
+    });
+    return true;
+}
+
+function backupDaily(){
+    if (!fs.existsSync(daily.dir)) {
+        fs.mkdirSync(daily.dir);
+    }
+    for (let key in daily.changes) {
+        fs.writeFileSync(daily.dir + key + '.json', JSON.stringify(daily.changes[key]));
+    }
+}
+
+function backupRuntime(){
+    fs.writeFileSync(config.backupValuesFile, JSON.stringify(valuesMap));
+    fs.writeFileSync(config.backupChangesFile, JSON.stringify(changesMap));
+}
+
+function quickBackup(){
     fs.writeFile(
-        config.backupFile, 
+        config.backupValuesFile, 
         JSON.stringify(valuesMap), 
-        function (err) {
-            if (err) {
-                console.log('Backup failed:' + err);
+        function (err1) {
+            if (err1) {
+                console.log('Backup values failed:' + err1);
             }
-            setTimeout(backup, config.backupInterval)
+            fs.writeFile(
+                config.backupChangesFile,
+                JSON.stringify(changesMap),
+                function (err2) {
+                    if (err2) {
+                        console.log('Backup values failed:' + err2);
+                    }
+                    setTimeout(quickBackup, config.backupInterval);
+                } 
+            )
         }
     );
 }
 
-setTimeout(backup, config.backupInterval);
+setTimeout(quickBackup, config.backupInterval);
 
 process.on('SIGINT', (code) => {
-    fs.writeFileSync(
-        config.backupFile,
-        JSON.stringify(valuesMap)
-    );
+    backupRuntime();
+    backupDaily();
     console.log('Process exit.')
     process.exit('SIGINT');
 });
